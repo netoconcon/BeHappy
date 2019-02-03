35a27680433a3a74d5060fd6e954ae73f3085c51
Neto Concon <concon.neto2@gmail.com>
2019-02-03 16:10:38 -0200

    update pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :project, :team]
+  skip_before_action :authenticate_user!, only: [:home, :project, :service, :metodologia, :team]
 
   def home
   end
 
   def project
+  end
+
+  def service
+  end
+
+  def metodologia
   end
 
   def team
