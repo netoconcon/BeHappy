f2fc0f4410d45838b5f388c6fc469c169cebdfeb
Neto Concon <concon.neto2@gmail.com>
2019-02-03 16:18:12 -0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'team', to: 'pages#team'
   get 'project', to: 'pages#project'
-  get 'condominios', to 'pages#condominios'
+  get 'condominios', to: 'pages#condominios'
   get 'startups', to: 'pages#startups'
   get 'metodologia', to: 'pages#metodologia'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
