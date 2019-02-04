af0168a7b5c1d20e2cb432b92f5f835d0e620c9c
Neto Concon <concon.neto2@gmail.com>
2019-02-04 18:51:51 -0200

    add dashobard to pages controller and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,4 +18,7 @@ class PagesController < ApplicationController
 
   def team
   end
+
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'condominios', to: 'pages#condominios'
   get 'startups', to: 'pages#startups'
   get 'metodologia', to: 'pages#metodologia'
+  get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
