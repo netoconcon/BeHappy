bf1d4fbbb2f2fedc1b965235879d307b07c28bfe
Neto Concon <concon.neto2@gmail.com>
2019-02-04 22:05:55 -0200

    add conflitos routes and call to action button

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'conflitos/index'
-  get 'conflitos/show'
-  get 'conflitos/new'
-  get 'conflitos/create'
-  get 'conflitos/edit'
-  get 'conflitos/update'
-  get 'conflitos/destroy'
   devise_for :users
   root to: 'pages#home'
   get 'team', to: 'pages#team'
@@ -14,6 +7,7 @@ Rails.application.routes.draw do
   get 'startups', to: 'pages#startups'
   get 'metodologia', to: 'pages#metodologia'
   get 'dashboard', to: 'pages#dashboard'
+  resources :conflitos
   # resources :prospects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
