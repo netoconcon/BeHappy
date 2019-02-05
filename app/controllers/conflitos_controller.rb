18d255e0d964ce88eee02065b912994ac5a7648e
Neto Concon <concon.neto2@gmail.com>
2019-02-04 22:03:08 -0200

    add conflito controller

--- app/controllers/conflitos_controller.rb
+++ app/controllers/conflitos_controller.rb
@@ -1,22 +1,40 @@
 class ConflitosController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @conflitos = Conflito.all
   end
 
   def show
+    @conflito = Conflito.find(params[:id])
   end
 
   def new
+    @conflit = Conflito.new
   end
 
   def create
+    @conflito = Conflito.new(conflito_params)
+    if @conflito.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @conflito = Conflito.find(params[:id])
   end
 
   def update
+    @conflito = Conflito.find(params[:id])
+    @conflito.update(conflito_params)
+    redirect_to conflito_path(@conflito)
   end
 
   def destroy
+    @conflito = conflito.find(params[:id])
+    @conflito.destroy
+    redirect_to conflitos_path
   end
 end
