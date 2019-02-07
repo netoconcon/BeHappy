1838842589eb34e8344fec0d7a297cbf43e5e380
Neto Concon <concon.neto2@gmail.com>
2019-02-07 21:51:23 -0200

    set welcome mail message

--- app/mailers/conflito_mailer.rb
+++ app/mailers/conflito_mailer.rb
@@ -6,7 +6,7 @@ class ConflitoMailer < ApplicationMailer
   #   en.conflito_mailer.welcome.subject
   #
   def welcome(conflito)
-    @conflito = conflito  # Instance variable => available in view
+    @conflito = conflito # Instance variable => available in view
 
     mail(to: 'zonadevalor01@gmail.com', subject: 'Novo Conflito na Zona de')
     # This will render a view in `app/views/user_mailer`!
