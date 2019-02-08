93bc2e4f376321f177f4c1752d579c2ced86bcbc
Neto Concon <concon.neto2@gmail.com>
2019-02-07 22:16:18 -0200

    add and set letter opener gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
