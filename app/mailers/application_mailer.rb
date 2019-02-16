03c928cc6e27976e8a5f81e004ea15fb3314a34f
Neto Concon <concon.neto2@gmail.com>
2019-02-16 19:24:30 -0200

    Revert "Mailing"

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'zonadevalor01@gmail.com'
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- app/models/conflito.rb
+++ app/models/conflito.rb
@@ -1,9 +1,2 @@
 class Conflito < ApplicationRecord
-    after_create :send_welcome_email
-
-  private
-
-  def send_welcome_email
-    ConflitoMailer.welcome(self).deliver_now
-  end
 end
